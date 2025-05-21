24b6cfc747e62862a9ab5fd06575be9915e808b8
gamemanuel <themathnight@gmail.com>
2025-05-21 16:36:49 -0700

    added structure for the Guns

--- robot.py
+++ robot.py
@@ -1,36 +1,61 @@
 import wpilib
 import wpilib.drive
 
-
 class MyRobot(wpilib.TimedRobot):
 
     def robotInit(self):
         """Robot initialization function"""
 
+        # <!-- start of drivetrain config -->
+        # leader motors
+        # frount left is motor 3
+        # frount right is motor 2
         leftfrountMotor = wpilib.PWMSparkMax(3)
         rightfrountMotor = wpilib.PWMSparkMax(2)
 
-        # Adding follower motors
+        # defining the follower motors
+        # back left is motor 1
+        # back left is motor 0
         leftFollowerMotor = wpilib.PWMSparkMax(1)
         rightFollowerMotor = wpilib.PWMSparkMax(0)
 
-        # Set followers to mirror the front motors
+        # set the follower motors to follow the leaders
         leftfrountMotor.addFollower(leftFollowerMotor)
         rightfrountMotor.addFollower(rightFollowerMotor)
 
         self.robotDrive = wpilib.drive.DifferentialDrive(leftfrountMotor, rightfrountMotor)
         self.driverController = wpilib.XboxController(0)
 
-        # We need to invert one side of the drivetrain so that positive voltages
-        # result in both sides moving forward. Depending on how your robot's
-        # gearbox is constructed, you might have to invert the left side instead.
+        # we need the right or left side to be inverted 
         rightfrountMotor.setInverted(True)
+        # <!-- End of Drivetrain -->
+
+        # <!-- Gun 1 Config -->
+        # TODO: add stuff here
+        # <!-- End of Gun 1 config -->
+        
+        # <!-- Gun 2 Config -->
+        # TODO: add stuff here
+        # <!-- End of Gun 2 config -->
+        
+        # <!-- Gun 3 Config -->
+        # TODO: add stuff here
+        # <!-- End of Gun 3 config -->
+        
+        # <!-- Gun 4 Config -->
+        # TODO: add stuff here
+        # <!-- End of Gun 4 config -->
+
+        # <!-- Gun 5 Config -->
+        # TODO: add stuff here
+        # <!-- End of Gun 5 config -->
+
+        # <!-- Gun 6 Config -->
+        # TODO: add stuff here
+        # <!-- End of Gun 6 config -->
 
     def teleopPeriodic(self):
-        # Drive with tank drive.
-        # That means that the Y axis of the left stick moves the left side
-        # of the robot forward and backward, and the Y axis of the right stick
-        # moves the right side of the robot forward and backward.
+        # set the motors to be in tank drive
         self.robotDrive.tankDrive(
             self.driverController.getLeftY(), self.driverController.getRightY()
         )
